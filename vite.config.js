ba9c9369eaee4351b19964e43e5a330de927dcd6
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-02-03 23:34:48 +0530

    reset to default

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DocsMini/",
 })
-// echo 'import { defineConfig } from "vite";  
-// import react from "@vitejs/plugin-react";  
-// export default defineConfig({  
-//   plugins: [react()],  
-//   base: "/", // FUCKING SIMPLE  
-// })' > vite.config.js  
